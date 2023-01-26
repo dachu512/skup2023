11661f0a0ad88fc770e086e54b226611ef92930c
Kamil Daszkiewicz <kamildaszkiewicz93@gmail.com>
2023-01-26 15:09:14 +0100

    Adding few changes

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
+const hiddenElements = document.querySelectorAll('.hidden');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if(entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
 
 navToggle.addEventListener('click', () => {
   primaryNav.toggleAttribute("data-visible");
 })
+hiddenElements.forEach((el) => observer.observe(el));
+
